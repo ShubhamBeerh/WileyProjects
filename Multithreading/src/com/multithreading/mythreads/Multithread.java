dd8e02ebc98b901d2f74ec92383873997c8f6e7f
ShubhamBeerh <shubhambeerh191@gmail.com>
2021-07-12 14:11:20 +0530

    Commit 1 for Non repeated Multithreading

--- Multithreading/src/com/multithreading/mythreads/Multithread.java
+++ Multithreading/src/com/multithreading/mythreads/Multithread.java
@@ -6,13 +6,15 @@ import java.util.List;
 
 public class Multithread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		MyThread1 m1 = new MyThread1();
 		Thread t1 = new Thread(m1);
 		Thread t2 = new Thread(m1);
 		t1.start();
+		Thread.sleep(10);
 		t2.start();
+		System.out.println(m1.getList());
 		
 	}
 
@@ -24,16 +26,12 @@ class MyThread1 implements Runnable{
 		// TODO Auto-generated method stub
 		list = new ArrayList<Integer>();
 		for(int i=0;i<=100;i++) {
-			if(Thread.currentThread().getName().equals("Thread-0")&&i<=50) {
+			if(!list.contains(i))
 				list.add(i);
-				System.out.println(Thread.currentThread().getName()+"-"+i);
-			}
-			else if(Thread.currentThread().getName().equals("Thread-1")&&i>50) {
-				list.add(i);
-				System.out.println(Thread.currentThread().getName()+"-"+i);
-			}
 		}
-		
 	}
+	public List<Integer> getList() {
+		return list;
+	}	
 	
 }
