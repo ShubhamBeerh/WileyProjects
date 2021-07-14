634973acd26e7338c4d1159f890b2f349f7a57e3
ShubhamBeerh <shubhambeerh191@gmail.com>
2021-07-14 10:59:46 +0530

    Fibonacci & Pattern using Synchronization,Volatile,Wait and Notify

--- Multithreading/src/com/multithreading/mythreads/Fibo.java
+++ Multithreading/src/com/multithreading/mythreads/Fibo.java
@@ -66,18 +66,21 @@ class F2 implements Runnable{
 	
 }
 class Series{
-	static int last;
+	static int last,lastSum=1,currentSum;
 	public Series(int last) {
 		this.last=last;
 	}
 	public void T1(List<Integer> list) throws InterruptedException{
-		while(list.size()!=10) {
+		while(list.size()!=6) {
 		synchronized(this) {
 			last++;
 			list.add(list.get(last-2)+list.get(last-1));
+			currentSum = list.get(last-2)+list.get(last-1);
+			System.out.println(lastSum+"+"+currentSum+"="+(lastSum+currentSum));
+			lastSum=lastSum+currentSum;
 			System.out.println(Thread.currentThread().getName()+list);
 			this.wait();
-			Thread.sleep(1000);
+			Thread.sleep(500);
 //			System.out.println("T1 Resumed");
 			this.notify();
 		}
@@ -87,14 +90,17 @@ class Series{
 	public void T2(List<Integer> list) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		Thread.sleep(500);
-		while(list.size()!=10) {
+		while(list.size()!=6) {
 			synchronized(this) {
 				last++;
 				list.add(list.get(last-2)+list.get(last-1));
+				currentSum = list.get(last-2)+list.get(last-1);
+				System.out.println(lastSum+"+"+currentSum+"="+(lastSum+currentSum));
+				lastSum=lastSum+currentSum;
 				notify();
 				System.out.println(Thread.currentThread().getName()+list);
 				this.wait();
-				Thread.sleep(1000);
+				Thread.sleep(500);
 //				System.out.println("T2 Resumed");
 			
 			}
